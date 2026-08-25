430c326d469027f4a776e52e334d0528e1e8053c
abdelmutalab-beep <abdelmutalab@gmail.com>
2026-08-25 12:28:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.GPSP_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbyEJWxcfotYapBZHcAVS5A3u_794YKTsb1IjqeBF3AyBUbwhzEdd18dkIfRgSWLGxSItA/exec",
-  GOOGLE_CLIENT_ID: "ضع هنا Client ID الكامل",
+  GOOGLE_CLIENT_ID: "869474469369-xxxxxxxxxxxxxxxx.apps.googleusercontent.com",
   APP_NAME: "GPSP 2.0 Premium",
   VERSION: "2.2.0"
 };
